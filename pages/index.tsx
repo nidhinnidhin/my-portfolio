36af4207d890d2f4d6ec83357c57fcae7309e4bd
Nidhin <nidhinbabu171@gmail.com>
2023-05-03 17:06:21 +0530

    new change

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Head>
-        <title>Nidhin's portfolio</title>
+        <title>Nidhin portfolio</title>
       </Head>
       <Header />
       <section id="hero" className="snap-start">
